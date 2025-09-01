041c05de11f55a45bdb17094211d51f421bb0d40
z-tier <fglemeric@gmail.com>
2025-09-01 09:17:43 +0800

    Dash works-- HEY WHY AM I NOT USING A STATE MACHINE
    Please hold.

--- broken-code/scripts/player.gd
+++ broken-code/scripts/player.gd
@@ -3,48 +3,56 @@ class_name Player extends CharacterBody2D
 
 @export var speed: float = 400.0
 @export var dash_cooldown: float = 1.5
-@export var dash_distance: float = 1000.0
+@export var dash_speed: float = 5000.0
+@export var dash_duration: float = 0.1
 
 
-var can_dash: bool = false
-var mouse_pos: Vector2 = Vector2.ZERO
+var can_dash: bool = true
+var is_dashing: bool = false
+var dash_dir: Vector2 = Vector2.ZERO
 
 
 @onready var dash_cooldown_timer: Timer = $DashCooldown
+@onready var dash_duration_timer: Timer = $DashTimer
 
 
 func _ready() -> void:
 	if not is_in_group("Player"):
 		add_to_group("Player")
+
 	dash_cooldown_timer.wait_time = dash_cooldown
-	dash_cooldown_timer.start()
+	dash_duration_timer.wait_time = dash_duration
 
 
 func _physics_process(delta: float) -> void:
-	mouse_pos = get_global_mouse_position()
+	var mouse_pos = get_global_mouse_position()
 	look_at(mouse_pos)
 
-	_handle_movement(delta)
+	if is_dashing:
+		velocity = dash_dir * dash_speed
+	else:
+		var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		velocity = dir.normalized() * speed
+
+		if Input.is_action_just_pressed("dash") and can_dash:
+			start_dash(dir, mouse_pos)
+
 	move_and_slide()
 
 
-func _handle_movement(delta: float) -> void:
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction.normalized() * speed
-
-	if Input.is_action_just_pressed("dash") and can_dash:
-		dash(direction, delta)
-
-
-func dash(direction: Vector2, delta: float) -> void:
+func start_dash(direction: Vector2, mouse_pos: Vector2) -> void:
 	can_dash = false
+	is_dashing = true
 
-	if direction == Vector2.ZERO:
-		global_position = to_local(mouse_pos).normalized() * dash_distance * delta
-	else:
-		global_position = direction.normalized() * dash_distance * delta
+	# dash towards mouse
+	dash_dir = (mouse_pos - global_position).normalized()
 
+	dash_duration_timer.start()
 	dash_cooldown_timer.start()
+
+
+func _on_dash_duration_timeout() -> void:
+	is_dashing = false
 
 
 func _on_dash_cooldown_timeout() -> void:
